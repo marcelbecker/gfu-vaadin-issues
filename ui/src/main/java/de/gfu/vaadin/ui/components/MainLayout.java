485ac3e1a2c36e66af7eaa8bcb312dcf2fec4a36
marcelbecker <marcel-becker@gmx.de>
2017-09-29 00:07:17 +0200

    finished VaadinData }> Ressourcen

--- ui/src/main/java/de/gfu/vaadin/ui/components/MainLayout.java
+++ ui/src/main/java/de/gfu/vaadin/ui/components/MainLayout.java
@@ -2,7 +2,10 @@ package de.gfu.vaadin.ui.components;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewDisplay;
+import com.vaadin.server.*;
 import com.vaadin.ui.*;
+
+import java.io.File;
 
 /**
  * Created by MBecker on 28.09.2017.
@@ -14,6 +17,23 @@ public class MainLayout extends CustomComponent implements ViewDisplay {
 
         panel.setContent(view.getViewComponent());
 
+
+        String basepath = VaadinService.getCurrent()
+                .getBaseDirectory().getAbsolutePath();
+
+        final FileResource fileResource =
+                new FileResource(new File(basepath + "/WEB-INF/images/vaadin.png"));
+        final ExternalResource externalResource =
+                new ExternalResource("http://www.gfu.net/");
+        final ThemeResource themeResource =
+                new ThemeResource("images/vaadin.png");
+
+
+        final Image fileImage = new Image(null, fileResource);
+        final HorizontalLayout horizontalLayout = new HorizontalLayout(fileImage,
+                new Link("GFU Cyrus", externalResource), new Image(null, themeResource));
+
+
         final MenuBar menuBar = new MenuBar();
 
         menuBar.addItem("Alle Issues",
@@ -23,9 +43,19 @@ public class MainLayout extends CustomComponent implements ViewDisplay {
         menuBar.addItem("Abmelden",
                 selectedItem -> getUI().getNavigator().navigateTo("logout"));
 
+        final Button fileDownload = new Button("Download File");
+        final Button themeImageDownload = new Button("Download Theme Image");
+        new FileDownloader(fileResource)
+                .extend(fileDownload);
+        new FileDownloader(themeResource)
+                .extend(themeImageDownload);
         setCompositionRoot(
                 new VerticalLayout(new Label("Oben"),
                         menuBar,
-                        panel, new Label("Unten")));
+                        panel,
+                        fileDownload,
+                        themeImageDownload,
+                        horizontalLayout,
+                        new Label("Unten")));
     }
 }
